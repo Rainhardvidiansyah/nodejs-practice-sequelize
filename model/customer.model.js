d7fc600737a7b17fbe747621289800d37bbf216c
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-03-13 02:11:31 +0700

    modify isActive datatypes from string to boolean

--- model/customer.model.js
+++ model/customer.model.js
@@ -1,4 +1,57 @@
-const {Sequelize, DataTypes} = require('sequelize')
+const { DataTypes } = require('sequelize')
+const sequelize = require('../databaseconfig/sequelize.config')
+const queryInterface = sequelize.sequelize.getQueryInterface()
+//const queryInterface = sequelize.getQueryInterface()
+
+const customerTable = sequelize.sequelize
+.define('customers', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+    username:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isActive:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+const modifyColum = queryInterface.changeColumn('customers', 'isActive', {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  });
 
 
-//insert into customers (id, username, password, email) value(0, '', '', '')
+async function customerSync(){
+    const create = await customerTable.sync()
+    console.log("Customers table has been created successfully!")
+}
+
+module.exports = {customerSync, modifyColum}
+
+
+
+
+
+
+
+
+
+// // const Customers = sequelize.sequelize.define('customers',{
+
+// // })
+
+
+// //insert into customers (id, username, password, email) value(0, '', '', '')

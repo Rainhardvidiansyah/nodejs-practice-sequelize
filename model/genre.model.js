8a8374c646adfde69e023b23221ae3976094eaa7
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-03-13 00:39:01 +0700

    create genre table

---
+++ model/genre.model.js
@@ -0,0 +1,27 @@
+const {DataTypes} = require('sequelize')
+
+const sequelize = require('../databaseconfig/sequelize.config')
+
+const Genre = sequelize.sequelize
+    .define('genre', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+async function syncGenre (){
+    try {
+        const create = await Genre.sync({force: true});
+    console.log("Table genre has been created successfully")
+    } catch (error) {
+        console.log(error)
+    }   
+}
+
+module.exports = {Genre, syncGenre}

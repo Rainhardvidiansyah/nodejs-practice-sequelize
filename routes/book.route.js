2246371230d71d2ad22a59bc507c823e5ee5f7bf
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-03-25 04:06:33 +0700

    create update route

--- routes/book.route.js
+++ routes/book.route.js
@@ -13,15 +13,14 @@ class BookRoute{
         this.route.post('/create', this.postBook);
         this.route.get('/all', this.getAllBook);
         this.route.get('/:id', this.getBookById)
+        this.route.put('/update/(:id)', this.updateBook)
     }
 
     async postBook(req, res){
         try {
         const {title, description, price, academicTitle, firstName, lastName} = req.body;
         const book = await bookController.insertBook(title, description, price, academicTitle, firstName, lastName);
-        // const bookId = await bookController.findBookById(book.id);
-        // console.log(bookId); use it later. in case you need to verify the book in database!
-        res.status(201).json({title, description, price, academicTitle, firstName, lastName});
+        res.status(201).json({message: "Book created successfully", data: book});
         } catch (error) {
             res.status(500).json(error.message);
         }}
@@ -45,11 +44,29 @@ class BookRoute{
         const id = req.params.id;
         await bookController.findBookById(id).then((book) => {
             if(book == null){
-            res.status(404).json({ message: "Book not found"})}
-                res.status(200).json(book)}
+            res.status(404)
+            .json({ message: "Book not found"})}
+            res.status(200).json(book)}
         ).catch(errors => console.log(errors))
         } catch (error) {
             throw new Error("Error: " + error)
+        }}
+    
+    async updateBook(req, res, next){
+        try{
+            const {title, description, price} = req.body;
+            const id = req.params.id;
+            const bookId = await bookController.findBookById(id);
+            if(bookId == null){
+               return res.status(404).json({
+                    message: "Book not found!"})
+                }
+            const book = await bookController.updateBook(id, title, description, price)
+            res.status(200).
+            json({message: "Success", data: [book]})
+            
+        } catch (error) {
+            res.status(500).json(error.message);
         }}
 
 
